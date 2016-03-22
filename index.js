d8dde47ec25753bc39e8b6142a4e9d370bb8b383
yangyxu <yangyxu@cisco.com>
2016-03-22 14:04:25 +0800

    update config file

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-var zn = require('./lib/zn.minx.js');
-var zn_data = require('./lib/zn-data.minx.js');
-module.exports = exports = zn;
-exports.data = zn_data;
+module.exports = exports = require('./lib/zn.minx.js');
